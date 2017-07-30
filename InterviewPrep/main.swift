5b1b2ad7c3e4db592061b48dd5c2f6e4d93c7a08
Amrutha Varshini <mantrarush@gmail.com>
2017-07-30 16:35:54 -0500

    DataStructures: Stack Implementation written in Swift.

---
+++ InterviewPrep/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  InterviewPrep
+//
+//  Created by Amrutha Varshini on 7/30/17.
+//  Copyright © 2017 mantrarush. All rights reserved.
+//
+
+import Foundation
+
+
+// Stack Usage
+stackUsageExamples()
