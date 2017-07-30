94f697ac36128346efae5f032bba90647a84ae63
Amrutha Varshini <mantrarush@gmail.com>
2017-07-30 17:26:58 -0500

    Updated source

--- InterviewQuestions/LinkedLists/CycleDetection.py
+++ InterviewQuestions/LinkedLists/CycleDetection.py
@@ -1,4 +1,4 @@
-# Check out the resources on the page's right side to learn more about linked lists. The video tutorial is by Gayle Laakmann McDowell, author of the best-selling interview book Cracking the Coding Interview.
+# HackerRank: https://www.hackerrank.com/challenges/ctci-linked-list-cycle
 # A linked list is said to contain a cycle if any node is visited more than once while traversing the list.
 #
 # Complete the function provided in the editor below. It has one parameter: a pointer to a Node object named  that points to the head of a linked list. Your function must return a boolean denoting whether or not there is a cycle in the list. If there is a cycle, return true; otherwise, return false.
@@ -19,3 +19,25 @@
 # Sample Input
 #
 # The following linked lists are passed as arguments to your function:
+
+
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as:
+
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+def has_cycles(head):
+    traversed_nodes = {head: True}
+
+    while head:
+        if traversed_nodes[head.next]:
+            return True
+        traversed_nodes[head.next] = True
+        head = head.next
+    return False
