98298367ccde4120ae1b84fd772c9dc0a8f02d58
Amrutha Varshini <mantrarush@gmail.com>
2017-08-13 16:31:46 -0500

    Refactor.

--- InterviewQuestions/DynamicProgramming/StringPermutationNoRep.py
+++ InterviewQuestions/DynamicProgramming/StringPermutationNoRep.py
@@ -1,13 +1,12 @@
-def permuteV2(input: str) -> [str]:
+def permute(input: str) -> [str]:
     if len(input) == 2:
         return [input, input[::-1]]
     characters = list(input)
     answers = []
-    for char in characters:
-        stringToPermuteNext = "".join(input.split(char))
-        permutations = permuteV2(stringToPermuteNext)
-        answers = answers + [char + permutation for permutation in permutations]
+    for character in characters:
+        permutations = permute("".join(input.split(character)))
+        answers = answers + [character + permutation for permutation in permutations]
 
     return answers
 
-print(permuteV2("antx"))
+print(permute("antx"))
